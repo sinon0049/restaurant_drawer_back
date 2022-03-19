d060050f16190e100a05547abb195f6962e44e4f
Me <sinon049@yahoo.com.tw>
2022-03-19 23:25:02 +0800

    feat: add CRUD features of work

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 const routes = require('./routes')
+const cors = require('cors')
 
+app.use(cors())
+app.use(express.json())
 app.use(routes)
 
 app.listen(port, () => {
--- routes/modules/work.js
+++ routes/modules/work.js
@@ -4,9 +4,35 @@ const db = require('../../models')
 const Work = db.Work
 
 router.get('/', (req, res) => {
-    console.log(req.method, req.route.path)
-    const works = Work.findAll()
-    res.send({ works })
+    return Work.findAll()
+        .then(works => {
+            return res.send({ works })
+        })
+})
+
+router.put('/:id', (req, res) => {
+    if(!req.body) return res.send({ status: 'error', message: 'no data transmitted' })
+    return Work.findByPk(req.params.id)
+        .then(work => work.update(req.body))
+        .then(() => res.send({ status: 'success', message: 'work update success' }))
+})
+
+router.post('/', (req, res) => {
+    if(!req.body) return res.send({ status: 'error', message: 'no data transmitted' })
+    return Work.create(req.body)
+        .then(() => res.send({ status: 'success', message: 'work update success' }))
+})
+
+router.delete('/:id', (req, res) => {
+    return Work.findByPk(req.params.id)
+        .then(work => work.destroy())
+        .then(() => res.send({ status: 'success', message: 'work delete success' }))
+})
+
+router.post('/done/:id', (req, res) => {
+    return Work.findByPk(req.params.id)
+        .then(work => work.update(req.body))
+        .then(() => res.send({ status: 'success', message: 'work done success' }))
 })
 
 module.exports = router
