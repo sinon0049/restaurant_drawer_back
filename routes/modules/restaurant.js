20722d84f3cbd3d2ec119cad192a7f24fadf3aa8
Me <sinon049@yahoo.com.tw>
2022-04-22 22:25:14 +0800

    feat: add userId verify feature in routes/restaurant.js

--- routes/modules/restaurant.js
+++ routes/modules/restaurant.js
@@ -5,35 +5,27 @@ const Restaurant = db.Restaurant
 const jwt = require('jsonwebtoken')
 const passport = require('passport')
 
-router.get('/', passport.authenticate('token', { session: false }), (req, res) => {
-    console.log(req.method, req.route.path)
+router.post('/history', passport.authenticate('token', { session: false }), async (req, res) => {
+    const userId = req.user.id
+    if(!req.body) return res.send({status: 'error', message: 'no data'})
+    const newRestaurant = { ...req.body, userId }
+    await Restaurant.create(newRestaurant)
+    return res.send({status: 'success', message: 'data send success'})
 })
 
-router.post('/history', passport.authenticate('token', { session: false }), (req, res) => {
-    console.log(req.method, req.route.path)
-    console.log(req.body)
-    const token = req.header('Authorization').replace('Bearer ', '')
-    jwt.verify(token, 'secret', async (err, decoded) => {
-        const userId = decoded.id
-        if(!req.body) return res.send({status: 'error', message: 'no data'})
-        const newRestaurant = { ...req.body, userId }
-        await Restaurant.create(newRestaurant)
-        return res.send({status: 'success', message: 'data send success'})
-    })
+router.get('/history', passport.authenticate('token', { session: false }), async (req, res) => {
+    const userId = req.user.id
+    const results = await Restaurant.findAll({ where: { userId }})
+    return res.send({results})
 })
 
-router.get('/history', passport.authenticate('token', { session: false }), (req, res) => {
-    return Restaurant.findAll()
-        .then(results => res.send({results}))
-})
-
-router.delete('/history/:id', passport.authenticate('token', { session: false }), (req, res) => {
-    return Restaurant.findByPk(req.params.id)
-        .then(restaurant => {
-            if(!restaurant) res.send({status: 'error', message: 'no target data'})
-            else restaurant.destroy()
-        })
-        .then(() => res.send({status: 'success', message: 'data delete success'}))
+router.delete('/history/:id', passport.authenticate('token', { session: false }), async (req, res) => {
+    const userId = req.user.id
+    const restaurant = await Restaurant.findByPk(req.params.id)
+    if(restaurant.dataValues.userId !== userId) return res.send({status: 'error', message: 'incorrect user'})
+    if(!Restaurant) return res.send({status: 'error', message: 'no target data'})
+    restaurant.destroy()
+    return  res.send({status: 'success', message: 'restaurant delete success'})
 })
 
 module.exports = router
