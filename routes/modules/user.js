48682f2c4593c94f1396acab391ef1aa9cdee614
Me <sinon049@yahoo.com.tw>
2022-11-30 15:58:56 +0800

    Add profile/password update feature and other minor changes

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -7,6 +7,17 @@ const jwt = require('jsonwebtoken')
 const passport = require('passport')
 const { OAuth2Client } = require('google-auth-library')
 
+async function getGoogleId(access_token) {
+    try {
+        const url = 'https://www.googleapis.com/oauth2/v3/userinfo'
+        const oauth2Client = new OAuth2Client()
+        oauth2Client.setCredentials({ access_token })
+        return (await oauth2Client.request({url})).data.sub
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 router.post('/signup', async (req, res) => {
     try {
         const sameUser = await User.findOne({ where: { email: req.body.email }})
@@ -43,7 +54,10 @@ router.post('/signin', passport.authenticate('local'), async (req, res) => {
             user: {
                 id: req.user.id,
                 email: req.user.email,
-                name: req.user.name
+                name: req.user.name,
+                facebookId: req.user.facebookId,
+                googleId: req.user.googleId,
+                isPwdSet: !!req.user.password ? true : false,
             }
         })
     } catch (error) {
@@ -64,7 +78,8 @@ router.post('/facebooksignin', async (req, res) => {
                 user: {
                     id: user.id,
                     email: user.email,
-                    name: user.name
+                    name: user.name,
+                    isPwdSet: user.dataValues.password ? true : false
                 }
             })
         } else {
@@ -81,10 +96,7 @@ router.post('/facebooksignin', async (req, res) => {
 
 router.post('/googlesignin', async (req, res) => {
     try {
-        const url = 'https://www.googleapis.com/oauth2/v3/userinfo'
-        const oauth2Client = new OAuth2Client()
-        oauth2Client.setCredentials({ access_token: req.body.access_token })
-        const googleId = (await oauth2Client.request({url})).data.sub
+        const googleId = await getGoogleId(req.body.access_token)
         const user = await User.findOne({ where: { googleId }})
         if(user) {
             const token = jwt.sign({ id: user.id }, process.env.SECRET)
@@ -95,7 +107,8 @@ router.post('/googlesignin', async (req, res) => {
                 user: {
                     id: user.id,
                     email: user.email,
-                    name: user.name
+                    name: user.name,
+                    isPwdSet: user.dataValues.password ? true : false
                 }
             })
         } else {
@@ -111,36 +124,74 @@ router.post('/googlesignin', async (req, res) => {
 })
 
 router.post('/oauthsignup', async (req, res) => {
-    const sameUser = await User.findOne({
-        where: {
-            email: req.body.email
-        }
-    })
-    if(sameUser) return res.json({
-        status: "error",
-        message: "Please sign in first and connect your account."
-    })
-    const randomPwd = Math.random().toString(36).slice(-8)
-    const salt = await bcrypt.genSalt(10)
-    const password = await bcrypt.hash(randomPwd, salt)
-    await User.create({
-        ...req.body,
-        password
-    })
-    return res.json({
-        status: "success",
-        ...req.body
-    })
+    try {
+        const sameUser = await User.findOne({
+            where: {
+                email: req.body.email
+            }
+        })
+        if(sameUser) return res.json({
+            status: "error",
+            message: "Please sign in first and connect your account."
+        })
+        await User.create({
+            ...req.body,
+            password: ''
+        })
+        return res.json({
+            status: "success",
+            ...req.body
+        })
+    } catch (error) {
+        console.log(error)
+    }
 })
 
-router.get('/get_current_user', passport.authenticate('token', { session: false }), (req, res) => {
-    const token = req.get('Authorization').replace('Bearer ', '')
-    jwt.verify(token, 'secret', async (err, decoded) => {
-        const decodedId = decoded.id
-        const user = await User.findByPk(decodedId, { raw: true })
-        const { id, name, email } = user
-        return res.json({ id, name, email })
-    })
+router.get('/current_user', passport.authenticate('token'), async (req, res) => {
+    try {
+        const userId = req.user.id
+        const user = await User.findByPk(userId, { raw: true })
+        return res.json({
+            ...user,
+            isPwdSet: user.password ? true : false
+        })
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.put('/profile', passport.authenticate('token'), async (req, res) => {
+    try {
+        if(req.body.access_token) {
+            const googleId = await getGoogleId(req.body.access_token)
+            req.body.googleId = googleId
+            delete req.body.access_token
+        }
+        const userId = req.user.id
+        const user = await User.findByPk(userId, {
+            attributes: ['id', 'name', 'email', 'facebookId', 'googleId']
+        })
+        await user.update(req.body)
+        return res.json({ status: "success", message: "profile updated successfully", user })
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+router.put('/password', passport.authenticate('token'), async (req, res) => {
+    try {
+        const payLoad = { ...req.body }
+        if(!payLoad.currentPwd.trim() && req.user.password) return res.json({ status: 'error', message: 'please type your password' })
+        if(req.user.password && !bcrypt.compareSync(payLoad.currentPwd, req.user.password)) return res.json({ status: 'error', message: 'incorrect password' })
+        if(payLoad.newPwd !== payLoad.confirmPwd) return res.json({ status: 'error', message: 'please confirm your password' })
+        const user = await User.findByPk(req.user.id)
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(payLoad.newPwd, salt)
+        await user.update({ password: hash })
+        return res.json({status: 'success', message: 'password updated successfully'})
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 module.exports = router
