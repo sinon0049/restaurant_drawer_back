9027d820c7f308172d77798b4e76658716dd4190
unknown <sinon049@yahoo.com.tw>
2023-02-22 13:33:17 +0800

    Update cors option

--- app.js
+++ app.js
@@ -5,8 +5,18 @@ const routes = require('./routes')
 const cors = require('cors')
 const usePassport = require('./config/passport')
 require('dotenv').config()
+const whiteList = ['https://todorest-715325.web.app', 'https://todorest-715325.firebaseapp.com', 'https://localhost:5173']
+const corsOption = {
+    origin: function (origin, callback) {
+        if (whiteList.indexOf(origin) !== -1) {
+          callback(null, true)
+        } else {
+          callback(new Error(`Origin ${origin} is not allowed by CORS policy`))
+        }
+    }
+}
 
-app.use(cors())
+app.use(cors(corsOption))
 app.use(express.json())
 usePassport(app)
 app.use(routes)
